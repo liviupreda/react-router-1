9f5996ded26d7bc725252b91a403984a64bb6418
Liviu Preda <liviupreda@gmail.com>
2019-11-25 11:35:26 +0200

    simulate routing with setState

--- src/App.js
+++ src/App.js
@@ -5,12 +5,33 @@ import Dog from "./Dog";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "about"
+    };
+    this.renderPage = this.renderPage.bind(this);
+  }
+
+  changePage(newPage) {
+    this.setState({ page: newPage });
+  }
+
+  renderPage() {
+    if (this.state.page === "about") return <About />;
+    else if (this.state.page === "dog") return <Dog />;
+    else if (this.state.page === "contact") return <Contact />;
+  }
+
   render() {
     return (
       <div className="App">
-        <About />
-        <Contact />
-        <Dog />
+        <nav>
+          <a onClick={() => this.changePage("about")}>About</a>
+          <a onClick={() => this.changePage("dog")}>Dog</a>
+          <a onClick={() => this.changePage("contact")}>Contact</a>
+        </nav>
+        {this.renderPage()}
       </div>
     );
   }
