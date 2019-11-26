40186d40b6eb1d9a89421217fdfc36bc20302d32
Liviu Preda <liviupreda@gmail.com>
2019-11-26 09:05:27 +0200

    install react-router-dom; move fake routing App.js to FakeRouter.js; new App.js to use with react router

--- src/App.js
+++ src/App.js
@@ -5,33 +5,10 @@ import Dog from "./Dog";
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      page: "about"
-    };
-    this.renderPage = this.renderPage.bind(this);
-  }
-
-  changePage(newPage) {
-    this.setState({ page: newPage });
-  }
-
-  renderPage() {
-    if (this.state.page === "about") return <About />;
-    else if (this.state.page === "dog") return <Dog />;
-    else if (this.state.page === "contact") return <Contact />;
-  }
-
   render() {
     return (
       <div className="App">
-        <nav className="App-nav">
-          <a onClick={() => this.changePage("about")}>About</a>
-          <a onClick={() => this.changePage("dog")}>Dog</a>
-          <a onClick={() => this.changePage("contact")}>Contact</a>
-        </nav>
-        <div>{this.renderPage()}</div>
+        <h1>Placeholder</h1>
       </div>
     );
   }
