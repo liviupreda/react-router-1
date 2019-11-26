c332320d795c503899b41b7576aa336c0f9e9357
Liviu Preda <liviupreda@gmail.com>
2019-11-26 10:02:58 +0200

    use Link comp instead of <a> tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import About from "./About";
 import Contact from "./Contact";
 import Dog from "./Dog";
@@ -11,6 +11,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className="App-nav">
+          <Link to="/">About</Link>
+          <Link to="/dog">Dog</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
         <Switch>
           <Route exact path="/" component={About} />
           <Route exact path="/dog" component={Dog} />
